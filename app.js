2ad0079805b69c5648118ad3b87bffc47f3316d7
Emmanuel Asamoah Botwe <noelnuel44@gmail.com>
2021-12-30 11:55:39 +0000

    updated routes controller

--- app.js
+++ app.js
@@ -5,10 +5,32 @@ const app = express();
 const path = require("path");
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
+const MongoClientConnection  = require('./utils/mongo-wrapper.js');
+require("dotenv").config();
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "./public")));
+
+const authenticateJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (authHeader) {
+        const token = authHeader.split(' ')[1];
+
+        jwt.verify(token, accessTokenSecret, (err, user) => {
+            if (err) {
+                return res.sendStatus(403);
+            }
+
+            req.user = user;
+            next();
+        });
+    } else {
+        res.sendStatus(401);
+    }
+};
 
 
 /**
@@ -24,6 +46,22 @@ app.post('/api/register', async (req, res) => {
         });
         return;
     }
+
+    // Filter user from the users array by username and password
+    // const user = users.find(u => { return u.username === username && u.password === password });
+
+    if (true) {
+        // Generate an access token
+        const accessToken = jwt.sign({ name: name,  email: email }, accessTokenSecret);
+
+        res.status(201).send({
+            token: accessToken
+        });
+    } 
+    // else {
+    //     res.send('Username or password incorrect');
+    // }
+
     res.status(201).send({
         token: 'token'
     });
@@ -39,8 +77,13 @@ app.post('/api/login', async (req, res) => {
 });
 
 const PORT = process.env.port || 8000;
-app.listen(PORT, ()=>{
-    console.log("Listening on port ")
-})
+let mongoClient = new MongoClientConnection();
+const accessTokenSecret = 'youraccesstokensecret';
+mongoClient.connect(process.env.MONGODB_URL).then(() => {
+    app.listen(PORT, ()=>{
+        console.log("Listening on port ", PORT)
+    })
+});
+
 
 module.exports = app;
